0db97b33c4bb8dfa8346de892ab2cfd6bbc7fbb1
Atsushi Toyama <atsushi.tohyama.160.333@gmail.com>
2022-12-09 23:57:59 +0900

    2022 day9

---
+++ 2022/day09/part2.go
@@ -0,0 +1,151 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func getInputFromFile(filename string) []string {
+	lines := []string{}
+
+	fp, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer fp.Close()
+
+	s := bufio.NewScanner(fp)
+
+	for s.Scan() {
+		lines = append(lines, s.Text())
+	}
+
+	if err = s.Err(); err != nil {
+		panic(err)
+	}
+
+	return lines
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func manhattanDist(x1, y1, x2, y2 int) int {
+	return abs(x1-x2) + abs(y1-y2)
+}
+
+func getNextTailMove(hX, hY, tX, tY int) (int, int) {
+	// Overlapping case
+	if hX == tX && hY == tY {
+		return tX, tY
+	}
+
+	md := manhattanDist(hX, hY, tX, tY)
+	if md <= 2 {
+		// Adjacent case
+		if md == 1 {
+			return tX, tY
+		}
+
+		// Diagonally adjacent case
+		if hX != tX && hY != tY {
+			return tX, tY
+		}
+
+		if hX == tX {
+			return tX, (hY + tY) / 2
+		}
+
+		return (tX + hX) / 2, tY
+	}
+
+	dX, dY := abs(hX-tX), abs(hY-tY)
+	if dX > dY {
+		return (hX + tX) / 2, hY
+	} else if dX < dY {
+		return hX, (hY + tY) / 2
+	}
+
+	// below case
+	//
+	//    | |s
+	//   -+-+-
+	//    | |
+	//   -+-+-
+	//   t| |
+	return (hX + tX) / 2, (hY + tY) / 2
+}
+
+const LEN = 1000
+
+func solvePart2(moves []byte, dists []int) {
+	visited := [LEN][LEN]bool{}
+
+	hX, hY := LEN/2, LEN/2
+	knotsX := [9]int{hX, hX, hX, hX, hX, hX, hX, hX, hX}
+	knotsY := [9]int{hY, hY, hY, hY, hY, hY, hY, hY, hY}
+	visited[hX][hY] = true
+
+	for i, move := range moves {
+		dist := dists[i]
+
+		// fmt.Printf("=== move %c, %d times ===\n", move, dist)
+
+		for j := 0; j < dist; j++ {
+			switch move {
+			case 'U':
+				hY += 1
+			case 'D':
+				hY -= 1
+			case 'L':
+				hX -= 1
+			case 'R':
+				hX += 1
+			default:
+				panic("Invalid move")
+			}
+
+			nextX, nextY := hX, hY
+			for i := 0; i < 9; i++ {
+				knotsX[i], knotsY[i] = getNextTailMove(nextX, nextY, knotsX[i], knotsY[i])
+				nextX, nextY = knotsX[i], knotsY[i]
+			}
+
+			// fmt.Printf("Head: (%d, %d), Tail: (%d, %d)\n", hX, hY, tX, tY)
+
+			visited[knotsX[8]][knotsY[8]] = true
+		}
+	}
+
+	count := 0
+	for i := 0; i < LEN; i++ {
+		for j := 0; j < LEN; j++ {
+			if visited[i][j] {
+				count += 1
+			}
+		}
+	}
+	fmt.Println(count)
+}
+
+func main() {
+	filename := "input"
+	input := getInputFromFile(filename)
+
+	moves := make([]byte, len(input))
+	dist := make([]int, len(input))
+
+	for i, line := range input {
+		moves[i] = line[0]
+		n, _ := strconv.Atoi(line[2:])
+		dist[i] = n
+	}
+
+	solvePart2(moves, dist)
+}
